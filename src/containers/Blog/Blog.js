0f708938cec4f916fdb6a7c1434f73a34b81e47c
Cristian <mateus_09@live.com>
2019-07-30 00:05:48 -0500

    Proyect finished

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import axios from "../../axios";
+
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: [],
+    selectedPost: null,
+    error: false
+  };
+
+  componentDidMount() {
+    axios.get("/posts").then(response => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map(post => {
+        return {
+          ...post,
+          author: "Cristian"
+        };
+      });
+      this.setState({ posts: updatedPosts });
+    }).catch(error => {
+        this.setState({error: true})
+    });
+  }
+
+  postSelectedHandler = id => {
+      this.setState({selectedPost: id})
+  };
+
+  render() {
+      let posts = <p style={{textAlign : 'center'}}>Something went wrong!</p>
+      if(!this.state.error){
+          posts = this.state.posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
+              />
+            );
+          });
+      }
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPost}/>
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,28 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import axios from 'axios'
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // edit request config
+    return request
+}, error => {
+    console.log(error);
+    return Promise.reject(error)
+})
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // Edit response config
+    return response
+}, error => {
+    console.log(error);
+    return Promise.reject(error)
+})
+
+axios.defaults.baseURL='http://jsonplaceholder.typicode.com'
+axios.defaults.headers.common['Authorization']='AUTH TOKEN'
+
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
